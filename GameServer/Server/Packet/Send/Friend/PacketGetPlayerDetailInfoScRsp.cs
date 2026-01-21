f13a81602f611c1638c7c181b1333de712c4fd33
zb1240610539 <zb1240610539@126.com>
2026-01-21 15:28:21 +0800

    Update PacketGetPlayerDetailInfoScRsp.cs

--- GameServer/Server/Packet/Send/Friend/PacketGetPlayerDetailInfoScRsp.cs
+++ GameServer/Server/Packet/Send/Friend/PacketGetPlayerDetailInfoScRsp.cs
@@ -2,11 +2,22 @@
 using EggLink.DanhengServer.Proto;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Friend;
-
 public class PacketGetPlayerDetailInfoScRsp : BasePacket
 {
+    // 原有的构造函数保留
     public PacketGetPlayerDetailInfoScRsp(PlayerDetailInfo info) : base(CmdIds.GetPlayerDetailInfoScRsp)
     {
+        var proto = new GetPlayerDetailInfoScRsp { DetailInfo = info };
+        SetData(proto);
+    }
+
+    // --- 核心修复：新增支持详细角色列表的构造函数 ---
+    public PacketGetPlayerDetailInfoScRsp(PlayerDetailInfo info, List<DisplayAvatarDetailInfo> displayList) 
+        : base(CmdIds.GetPlayerDetailInfoScRsp)
+    {
+        // 关键：PlayerDetailInfo 里的 DisplayAvatarList 才是控制详情弹窗的数据源
+        info.DisplayAvatarList.AddRange(displayList);
+
         var proto = new GetPlayerDetailInfoScRsp
         {
             DetailInfo = info
@@ -17,11 +28,7 @@ public class PacketGetPlayerDetailInfoScRsp : BasePacket
 
     public PacketGetPlayerDetailInfoScRsp() : base(CmdIds.GetPlayerDetailInfoScRsp)
     {
-        var proto = new GetPlayerDetailInfoScRsp
-        {
-            Retcode = 3612
-        };
-
+        var proto = new GetPlayerDetailInfoScRsp { Retcode = 3612 };
         SetData(proto);
     }
 }
