162d167dd4fa51889d284da785f8b6b96744de21
zb1240610539 <zb1240610539@126.com>
2026-01-23 16:00:13 +0800

    Update HandlerTakeTrialActivityRewardCsReq.cs 试用活动奖励

--- GameServer/Server/Packet/Recv/Activity/HandlerTakeTrialActivityRewardCsReq.cs
+++ GameServer/Server/Packet/Recv/Activity/HandlerTakeTrialActivityRewardCsReq.cs
@@ -1,4 +1,4 @@
-﻿using EggLink.DanhengServer.Data;
+using EggLink.DanhengServer.Data;
 using EggLink.DanhengServer.Database.Activity;
 using EggLink.DanhengServer.Database.Inventory;
 using EggLink.DanhengServer.GameServer.Server.Packet.Send.Activity;
@@ -6,48 +6,58 @@ using EggLink.DanhengServer.GameServer.Server.Packet.Send.PlayerSync;
 using EggLink.DanhengServer.GameServer.Server.Packet.Send.Scene;
 using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
+using EggLink.DanhengServer.Database;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Recv.Activity;
 
 [Opcode(CmdIds.TakeTrialActivityRewardCsReq)]
 public class HandlerTakeTrialActivityRewardCsReq : Handler
 {
- public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
+    public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
     {
         var req = TakeTrialActivityRewardCsReq.Parser.ParseFrom(data);
         var player = connection.Player!;
 
-        GameData.AvatarDemoConfigData.TryGetValue((int)req.StageId, out var stage);
-        if (stage != null)
+        // 1. 获取试用关卡和奖励配置
+        if (!GameData.AvatarDemoConfigData.TryGetValue((int)req.StageId, out var stage)) return;
+        if (!GameData.RewardDataData.TryGetValue(stage.RewardID, out var reward)) return;
+
+        // 2. 构造奖励列表
+        var rewardItems = reward.GetItems().Select(x => new ItemData { ItemId = x.Item1, Count = x.Item2 }).ToList();
+        
+        // 如果有星琼奖励，加入列表。AddItems 内部会自动处理 Player.Data.Hcoin 的增加
+        if (reward.Hcoin > 0)
         {
-            GameData.RewardDataData.TryGetValue(stage.RewardID, out var reward);
-            var itemList = new List<ItemData>();
-
-            var rewardItems = reward?.GetItems() ?? new List<(int, int)>();
-            foreach (var i in rewardItems)
-            {
-                // 修改点：将 sync 设为 false。
-                // 这样 AddItem 内部就不会去加 Hcoin，也不会重复发同步包。
-                var res = await player.InventoryManager!.AddItem(i.Item1, i.Item2, false, sync: false);
-                if (res != null) itemList.Add(res);
-            }
-
-            var activities = player.ActivityManager!.Data.TrialActivityData.Activities;
-            var activityIndex = activities.FindIndex(x => x.StageId == req.StageId);
-            if (activityIndex != -1)
-            {
-                activities[activityIndex] = new TrialActivityResultData
-                {
-                    StageId = (int)req.StageId,
-                    TakenReward = true
-                };
-            }
-
-            
-            // 保留所有原始发包逻辑
-            await player.SendPacket(new PacketPlayerSyncScNotify(player.ToProto(), itemList));
-            await player.SendPacket(new PacketScenePlaneEventScNotify(itemList));
-            await connection.SendPacket(new PacketTakeTrialActivityRewardScRsp(req.StageId, itemList));
+            rewardItems.Add(new ItemData { ItemId = 1, Count = (int)reward.Hcoin });
         }
+
+        // 3. 批量添加物品
+        // AddItems 内部会处理数据逻辑并统一发送物品同步包
+        var itemList = await player.InventoryManager!.AddItems(rewardItems, notify: true);
+
+        // 4. 更新试用活动进度
+        var trialData = player.ActivityManager!.Data.TrialActivityData;
+        var activityIndex = trialData.Activities.FindIndex(x => x.StageId == req.StageId);
+        if (activityIndex != -1)
+        {
+            trialData.Activities[activityIndex] = new TrialActivityResultData
+            {
+                StageId = (int)req.StageId,
+                TakenReward = true
+            };
+        }
+
+       
+
+        // 6. 核心同步：刷新顶栏 UI
+        // 因为 AddItems 内部对虚拟物品使用了静默模式，所以这里必须手动补发一个全量玩家数据包
+        // 这样客户端左上角的星琼余额才会立刻刷新
+        await player.SendPacket(new PacketPlayerSyncScNotify(player.ToProto()));
+
+        // 7. 发送响应包，结束客户端的转圈等待
+        await connection.SendPacket(new PacketTakeTrialActivityRewardScRsp(req.StageId, rewardItems));
+
+        // 8. 全量同步活动状态，防止逻辑死锁
+        await player.ActivityManager.SyncTrialActivity();
     }
 }
