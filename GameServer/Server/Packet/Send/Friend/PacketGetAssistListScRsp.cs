9ecbed86c50793130a9aca0c4fb127701278f621
zb1240610539 <zb1240610539@126.com>
2026-01-26 09:45:34 +0800

    Update PacketGetAssistListScRsp.cs 助战修复

--- GameServer/Server/Packet/Send/Friend/PacketGetAssistListScRsp.cs
+++ GameServer/Server/Packet/Send/Friend/PacketGetAssistListScRsp.cs
@@ -1,14 +1,13 @@
-﻿using EggLink.DanhengServer.Kcp;
+using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Friend;
 
 public class PacketGetAssistListScRsp : BasePacket
 {
-    public PacketGetAssistListScRsp() : base(CmdIds.GetAssistListScRsp)
+    // 修改构造函数，允许传入已经填充好数据的 Proto 对象
+    public PacketGetAssistListScRsp(GetAssistListScRsp proto) : base(CmdIds.GetAssistListScRsp)
     {
-        var proto = new GetAssistListScRsp();
-
         SetData(proto);
     }
 }
