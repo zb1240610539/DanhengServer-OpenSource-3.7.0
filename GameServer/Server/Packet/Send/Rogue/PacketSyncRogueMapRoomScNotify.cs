05a32b29c9abe394aa6c16875db926c03bc28fa4
zb1240610539 <zb1240610539@126.com>
2026-01-29 16:03:25 +0800

    Update PacketSyncRogueMapRoomScNotify.cs 模拟宇宙进度更新

--- GameServer/Server/Packet/Send/Rogue/PacketSyncRogueMapRoomScNotify.cs
+++ GameServer/Server/Packet/Send/Rogue/PacketSyncRogueMapRoomScNotify.cs
@@ -6,24 +6,16 @@ namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Rogue;
 
 public class PacketSyncRogueMapRoomScNotify : BasePacket
 {
-    // 模仿 LunarCore 的逻辑，传入 Rogue 实例数据和房间数据
-    public PacketSyncRogueMapRoomScNotify(uint levelId, uint mapId, RogueRoom roomProto) : base(CmdIds.SyncRogueMapRoomScNotify)
+    public PacketSyncRogueMapRoomScNotify(RogueRoomInstance room, int mapId) : base(CmdIds.SyncRogueMapRoomScNotify)
     {
-        // 处理老版本混淆字段
-        // 猜测：在老版本中，room_info 内部的 BEEEBOIOJIF (Tag 8) 必须同步 CurStatus
-        // 否则客户端地图不会更新房间的“已完成”视觉状态
-        roomProto.BEEEBOIOJIF = roomProto.CurStatus; 
-
-        // 猜测：IMIMGFAAGHM (Tag 5) 可能需要对应房间的位置索引或 SiteId
-        if (roomProto.IMIMGFAAGHM == 0) {
-            roomProto.IMIMGFAAGHM = roomProto.SiteId;
-        }
-
         var proto = new SyncRogueMapRoomScNotify
-        {
-            LevelId = levelId, // Tag 1
-            MapId = mapId,     // Tag 2
-            RoomInfo = roomProto // Tag 3 (对应老版本的 room_info)
+        {   roomProto.BEEEBOIOJIF = roomProto.CurStatus;
+        	// 猜测：IMIMGFAAGHM (Tag 5) 可能需要对应房间的位置索引或 SiteId
+        	if (roomProto.IMIMGFAAGHM == 0) {
+            roomProto.IMIMGFAAGHM = roomProto.SiteId;
+        	}
+            CurRoom = room.ToProto(),
+            MapId = (uint)mapId
         };
 
         SetData(proto);
